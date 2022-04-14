92198f1f33efff464aadf1435a3d3fa1bc673ccd
Jonathan Wang <youchun2@illinois.edu>
2022-04-13 20:13:34 -0500

    Create week1 branch for weekly pull request.

--- include/flappybird_simulation_app.h
+++ include/flappybird_simulation_app.h
@@ -30,8 +30,8 @@ public:
    */
   void keyDown(ci::app::KeyEvent event) override;
 
-  const int kWindowSizeX = 1250; /// The pop-up window width
-  const int kWindowSizeY = 725; /// The pop-up window height
+  const int kWindowSizeX = 1250; /// the pop-up window width
+  const int kWindowSizeY = 725; /// the pop-up window height
 
 private:
   GameEngine game_engine_;
